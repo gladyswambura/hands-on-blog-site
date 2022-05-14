0c4d734398bbe0b47ff5329798f17e21364b9908
gladyswambura <gladyswahito7@gmail.com>
2022-05-14 15:38:10 +0300

    creates migrations by installing flask-migrate

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 from app import create_app
 from flask_script import Manager, Server
-
+from  flask_migrate import Migrate, MigrateCommand
 
 
 app=create_app('production')
 manager=Manager(app)
 manager.add_command('server', Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 if __name__ == '__main__':
     manager.run()
