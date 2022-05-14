8a6bec47f095c8681f293a64d8a268d93819a441
gladyswambura <gladyswahito7@gmail.com>
2022-05-14 11:37:37 +0300

    imports Boottsrap and creates its instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
 
+
+app=Flask(__name__)
+bootstrap = Bootstrap(app)
 
 def create_app(config_name):
-    app=Flask(__name__)
-
-
     app.config.from_object(config_options[config_name])
+
 
 
     from .auth import auth as auth_blueprint
