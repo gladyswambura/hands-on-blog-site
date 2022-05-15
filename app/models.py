4d098c7de977663f9497d9be4290bc95b4eded0e
gladyswambura <gladyswahito7@gmail.com>
2022-05-15 09:09:54 +0300

    creates a new class for subscribers

--- app/models.py
+++ app/models.py
@@ -39,12 +39,12 @@ class Blog(db.Model):
         db.session.commit()
 
     @classmethod
-    def get_user_posts(cls,id):
+    def get_user_blogs(cls,id):
         posts = Blog.query.filter_by(user_id = id).order_by(Blog.posted_at.desc()).all()
         return posts
 
     @classmethod
-    def get_all_posts(cls):
+    def get_all_blogs(cls):
         return Blog.query.order_by(Blog.posted_at).all()
 
     def __repr__(self):
@@ -75,62 +75,10 @@ class Comment(db.Model):
         return f'Comments: {self.comment}'
 
 
-class Upvote(db.Model):
-    __tablename__ = 'upvotes'
-    id = db.Column(db.Integer, primary_key=True)
-    upvote = db.Column(db.Integer, default=1)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-
-    @classmethod
-    def query_upvotes(cls, id):
-        upvote = Upvote.query.filter_by(blog_id=id).all()
-        return upvote
-
-    @classmethod
-    def all_upvotes(cls):
-        upvotes = Upvote.query.order_by('id').all()
-        return upvotes
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.blog_id}'
-
-class Downvote(db.Model):
-    __tablename__ = 'downvotes'
-    id = db.Column(db.Integer, primary_key=True)
-    downvote = db.Column(db.Integer, default=1)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-
-    @classmethod
-    def query_downvotes(cls, id):
-        downvote = Downvote.query.filter_by(blog_id=id).all()
-        return downvote
-
-    @classmethod
-    def all_downvotes(cls):
-        downvote = Downvote.query.order_by('id').all()
-        return downvote
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.blog_id}'
-
-
-
-
-
-
-
+class Subscribers(db.Model):
+    __tablename__ = "subscribers"
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(255), unique = True, index = True)
 
     
 
